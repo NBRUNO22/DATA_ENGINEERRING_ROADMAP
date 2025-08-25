d3ac8d1fe30a3cd3bb5e59ccb69f9f7629a4f4d8
Bruno Ribeiro <nbruno22@hotmail.com>
2025-08-25 10:56:25 -0300

    Organizando pastas

--- desafio.py
+++ desafio.py
@@ -6,7 +6,7 @@ nome_usuario = input("Digite o seu nome: ")
 # 2) Solicita ao usuário que digite o valor do seu salário
 # Converte a entrada para um número de ponto flutuante
 salario_usuario = float(input("Digite o seu salario: "))
-
+   
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
 bonus_usuario = float(input("Digite o seu bonus: "))
@@ -21,5 +21,5 @@ print(f"O usuario {nome_usuario} possui o bonus de {valor_do_bonus}")
 
 # Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
 
-# o nome pode ser numero e onde esta numero pode colocar string, 
+# adicionar tratamento de erros  nome pode ser numero e onde esta numero pode colocar string, 
 # valor pode colocar negativo o que influencia no bonus5
